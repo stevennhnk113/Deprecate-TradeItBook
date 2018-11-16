344832a1cbeb3e84c5308f68b58e22227aa65c25
Nguyen Nguyen <stevennhnk113@gmail.com>
2018-11-15 20:17:41 -0800

    Remove uneccesary files

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -8,7 +8,6 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Hello} from './Hello';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
