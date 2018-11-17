0d4dce60589722e75ccf1e480036339dc69bd41b
Steven <stevennhnk113@gmail.com>
2018-11-17 13:53:41 -0800

    Remove aws amplify UI component

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -10,46 +10,17 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import { withAuthenticator, Authenticator, SignIn, SignUp, ConfirmSignUp, Greetings } from 'aws-amplify-react-native';
-Amplify.configure(aws_exports);
 
-const instructions = Platform.select({
-	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-	android:
-		'Double tap R on your keyboard to reload,\n' +
-		'Shake or press menu button for dev menu helo',
-});
+Amplify.configure(aws_exports);
 
 type Props = {};
 
 export default class App extends Component<Props> {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Authenticator
-					onStateChange={(authState) => console.log(authState)}
-					amplifyConfig={aws_exports} >
-				</Authenticator>
+			<View>
+				<Text>Helo world</Text>
 			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
-});
