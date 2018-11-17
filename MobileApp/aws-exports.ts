44bb192aff3714acb63434f14c1b03e882f79782
Nguyen Nguyen <stevennhnk113@gmail.com>
2018-11-16 23:44:54 -0800

    Adding authentication

--- MobileApp/aws-exports.ts
+++ MobileApp/aws-exports.ts
@@ -4,6 +4,8 @@ const awsmobile =  {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "us-east-1:52c5ab4c-0d20-441c-b175-bc270742a89c",
     "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_sOlLtHYuT",
+    "aws_user_pools_web_client_id": "6d5re64peqsm9cv7kgratk7asj",
     "aws_mobile_analytics_app_id": "fb028b25e2304a0282d87457973da923",
     "aws_mobile_analytics_app_region": "us-east-1",
     "aws_appsync_graphqlEndpoint": "https://bjam5cy6ebg7jl3nupk3arbgmu.appsync-api.us-east-1.amazonaws.com/graphql",
